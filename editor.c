422691bff3fc528c52882d8890b7fe40fe705bbe
Johnathan Middleton <johnathanmiddleton1@gmail.com>
2022-04-02 16:36:32 -0400

    Allow user to open a file, and display the first line within editor

--- editor.c
+++ editor.c
@@ -1,4 +1,9 @@
 /*** included files ***/
+
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -308,17 +313,30 @@ void editorProcessKeypress()
 
 /*** file input and output ***/
 
-void editorOpen()
+void editorOpen(char *filename)
 {
-    char* line = "Hello world!";
+    FILE *fp = fopen(filename, "r");
+    if (!fp) die("fopen");
 
-    ssize_t linelen = strlen(line);
-
-    E.row.size = linelen;
-    E.row.chars = malloc(linelen + 1);
-    memcpy(E.row.chars, line, linelen);
-    E.row.chars[linelen] = '\0';
-    E.numrows = 1;
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    linelen = getline(&line, &linecap, fp);
+    if (linelen != -1)
+    {
+        while (linelen > 0 && (line[linelen - 1] == '\n' ||
+                               line[linelen - 1] == '\r'))
+        {
+            linelen--;
+        }
+        E.row.size = linelen;
+        E.row.chars = malloc(linelen + 1);
+        memcpy(E.row.chars, line, linelen);
+        E.row.chars[linelen] = '\0';
+        E.numrows = 1;
+    }
+    free(line);
+    fclose(fp);
 }
 
 /*** append buffer ***/
@@ -464,11 +482,15 @@ void initEditor()
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
-int main ()
+int main(int argc, char *argv[])
 {
     enableRawMode();
     initEditor();
-    editorOpen();
+
+    if (argc >= 2)
+    {
+        editorOpen(argv[1]);
+    }
 
     while (1)  
     {
