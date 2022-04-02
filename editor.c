c030f7352e35be65303552dc77dca9a24c11beee
Johnathan Middleton <johnathanmiddleton1@gmail.com>
2022-04-02 01:00:41 -0400

    Handle page up and down for going to top and bottom, home for left of window, end for right

--- editor.c
+++ editor.c
@@ -23,6 +23,8 @@ enum editorKey {
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    HOME_KEY,
+    END_KEY,
     PAGE_UP,
     PAGE_DOWN
 };
@@ -132,8 +134,12 @@ int editorReadKey()
                 {
                     switch (seq[1])
                     {
-                        case '1': return PAGE_UP;
-                        case '2' : return PAGE_DOWN;
+                        case '1': return HOME_KEY;
+                        case '4': return END_KEY;
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;
                     }
                 }
             }
@@ -146,7 +152,17 @@ int editorReadKey()
                     case 'B': return ARROW_DOWN;
                     case 'C': return ARROW_RIGHT;
                     case 'D': return ARROW_LEFT;
+                    case 'H': return HOME_KEY;
+                    case 'F': return END_KEY;
                 }
+            }
+        }
+        else if (seq[0] == 'O')
+        {
+            switch (seq[1])
+            {
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
             }
         }
 
@@ -251,6 +267,15 @@ void editorProcessKeypress()
             exit(0);
             break;
 
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+
+        case END_KEY:
+            E.cx = E.screencols - 1;
+            break;
+
+        // TODO:handling page up after multiple presses causes weird behavior
         case PAGE_UP:
         case PAGE_DOWN:
             {
